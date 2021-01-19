fb095b13183dc70fb2d2b78547e3500597b73f59
Steven Zhu <stevenzhu94@users.noreply.github.com>
2021-01-19 15:21:07 -0600

    refactored and optimized calculations for speed, and reducedvehicle detection rate to once per second

--- FinalCode/speed.py
+++ FinalCode/speed.py
@@ -13,6 +13,8 @@ video = cv2.VideoCapture('../Videos/default.mp4')
 
 WIDTH = 1280
 HEIGHT = 720
+# WIDTH = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+# HEIGHT = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 # def estimateSpeed(location1, location2):
 #     d_pixels = math.sqrt(math.pow(location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
@@ -28,25 +30,29 @@ def estimateSpeed(location1, location2, lineTracker, image, cnts, resultImage):
     d_pixels = distanceFormula(location1[0], location1[1], location2[0], location2[1])
     closestLine = findClosestLine(lineTracker, location2)
 
-    #find closest line ppm
+    # Use closest traffic line to measure pixel per metric
     metric = 3
     ppm = getPixelPerMetric(closestLine, cnts, metric, resultImage, location2)
     if (ppm == None):
         return None
 
     d_meters = d_pixels / ppm
-    fps = video.get(cv2.CAP_PROP_FPS)
+    fps = getFPS()
     speed = d_meters * fps * 3.6
     # print("ppm: ", ppm, "| d_mtrs: ", d_meters, "| spd: ", speed)
+
     return speed
 
 def distanceFormula(x1, y1, x2, y2):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
+def getFPS():
+    return video.get(cv2.CAP_PROP_FPS)
+
 def getTrafficLines(image):
     lineTracker = {}
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    lines = lineCascade.detectMultiScale(gray, 1.1, 4, 10, (1, 1),(6,6))
+    lines = lineCascade.detectMultiScale(gray, 1.1, 4, 10, (4, 4), (6,6))
     lineID = 0
     for (_x, _y, _w, _h) in lines:
         x = int(_x)
@@ -60,7 +66,9 @@ def getTrafficLines(image):
 def findClosestLine(lineTracker, carLocation):
     closest = 10000000000000
     closestLine = None
-    carX = carLocation[0]
+
+    # (carX, carY) reflects the midpoint on the bottom bar of vehicle rectangle
+    carX = carLocation[0] + carLocation[2] / 2
     carY = carLocation[1] + carLocation[3]
     for lineID in lineTracker.keys():
         lineXCenter = lineTracker[lineID][0] + lineTracker[lineID][2] / 2
@@ -71,22 +79,24 @@ def findClosestLine(lineTracker, carLocation):
             closestLine = lineTracker[lineID]
     return closestLine
 
-def testLines(lines, image, resultImage):
+def showTrafficLines(lines, image, resultImage):
     for lineID in lines.keys():
-        t_x = lines[lineID][0]
-        t_y = lines[lineID][1]
-        t_w = lines[lineID][2]
-        t_h = lines[lineID][3]
-        cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), (0, 255, 255), 2)
+        t_x, t_y, t_w, t_h = lines[lineID]
+        cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), (0, 255, 255), 1)
 
 def getPixelPerMetric(line, cnts, metric, resultImage, carLocation):
-    # find contour belonging to line param
+    # find contour of closest detected line
     lx, ly, lw, lh = line
     lineContour = None
+    box = None
+
     for c in cnts:
         cx, cy, cw, ch = cv2.boundingRect(c)
         if (lx < cx and ly < cy and (lx+lw) > (cx+cw) and (ly+lh) > (cy+ch)):
             lineContour = cv2.boundingRect(c)
+            rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
             break
 
     if (lineContour == None):
@@ -94,8 +104,13 @@ def getPixelPerMetric(line, cnts, metric, resultImage, carLocation):
     else:
         x, y, w, h = lineContour
 
-        cv2.line(resultImage, (x,y), (carLocation[0],carLocation[1]+carLocation[3]), (255,0,0), 2)
-        cv2.line(resultImage, (x,y), (x+w, y+h), (255,255,0), 2)
+        # This draws the detected traffic line and and a line leading to it from the car
+        cv2.drawContours(resultImage, [box], 0, (255, 255, 0), 2)
+        if (distanceFormula(carLocation[0], carLocation[1]+carLocation[3], box[0][0], box[0][1]) < 
+            distanceFormula(carLocation[0]+carLocation[2], carLocation[1]+carLocation[3], box[0][0], box[0][1])):
+            cv2.line(resultImage, (box[0][0],box[0][1]), (carLocation[0], carLocation[1]+carLocation[3]), (255,0,0), 2)
+        else:
+            cv2.line(resultImage, (box[0][0],box[0][1]), (carLocation[0]+carLocation[2], carLocation[1]+carLocation[3]), (255,0,0), 2)
 
         pixelsPerMetric = distanceFormula(x, y, x+w, y+h) / metric
         return pixelsPerMetric
@@ -107,7 +122,7 @@ def getContourMap(image):
 
     # perform edge detection, then perform a dilation + erosion to
     # close gaps in between object edges
-    edged = cv2.Canny(gray, 25, 100)
+    edged = cv2.Canny(gray, 50, 100)
     edged = cv2.dilate(edged, None, iterations=1)
     edged = cv2.erode(edged, None, iterations=1)
 
@@ -120,123 +135,111 @@ def getContourMap(image):
     return cnts
 
 def trackMultipleObjects():
+    fps = getFPS()
     rectangleColor = (0, 255, 0)
     frameCounter = 0
     currentCarID = 0
 
     carTracker = {}
     carNumbers = {}
-    carLocation1 = {}
-    carLocation2 = {}
-    speed = [None] * 1000
+    carLocation = {}
+    speed = {}
 
     cnts = None
+    lineTracker = None
 
     # Write output to video file
     out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (WIDTH, HEIGHT))
 
     while True:
-        rc, image = video.read()
+        src, image = video.read()
+
         if type(image) == type(None):
             break
 
         image = cv2.resize(image, (WIDTH, HEIGHT))
         resultImage = image.copy()
 
-        frameCounter = frameCounter + 1
-
-        carIDtoDelete = []
-
-        for carID in carTracker.keys():
+        # Remove low quality tracked vehicles
+        for carID in list(carTracker):
             trackingQuality = carTracker[carID].update(image)
+            if trackingQuality < 6:
+                print('Removing low quality tracker ' + str(carID))
+                del carTracker[carID]
+                del carLocation[carID]
 
-            if trackingQuality < 7:
-                carIDtoDelete.append(carID)
+        # Identifying the vehicles, traffic lines, and contour lines are expensive so we limit it to once per second
+        # Map identified vehicles to ones we are already tracking, then add new trackers for the ones we are not
+        if (frameCounter % fps == 0):
+            cnts = getContourMap(image)
+            lineTracker = getTrafficLines(image)
 
-        for carID in carIDtoDelete:
-            print('Removing carID ' + str(carID) + ' from list of trackers.')
-            carTracker.pop(carID, None)
-            carLocation1.pop(carID, None)
-            carLocation2.pop(carID, None)
-        
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cars = carCascade.detectMultiScale(gray, 1.1, 16, 18, (25, 25))
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            cars = carCascade.detectMultiScale(gray, 1.1, 20, 18, (25, 25))
 
-        for (_x, _y, _w, _h) in cars:
-            x = int(_x)
-            y = int(_y)
-            w = int(_w)
-            h = int(_h)
+            for (_x, _y, _w, _h) in cars:
+                x = int(_x)
+                y = int(_y)
+                w = int(_w)
+                h = int(_h)
 
-            x_bar = x + 0.5 * w
-            y_bar = y + 0.5 * h
+                x_bar = x + 0.5 * w
+                y_bar = y + 0.5 * h
 
-            matchCarID = None
+                matchCarID = None
 
-            for carID in carTracker.keys():
-                trackedPosition = carTracker[carID].get_position()
+                for carID in carTracker.keys():
+                    trackedPosition = carTracker[carID].get_position()
 
-                t_x = int(trackedPosition.left())
-                t_y = int(trackedPosition.top())
-                t_w = int(trackedPosition.width())
-                t_h = int(trackedPosition.height())
+                    t_x = int(trackedPosition.left())
+                    t_y = int(trackedPosition.top())
+                    t_w = int(trackedPosition.width())
+                    t_h = int(trackedPosition.height())
 
-                t_x_bar = t_x + 0.5 * t_w
-                t_y_bar = t_y + 0.5 * t_h
+                    t_x_bar = t_x + 0.5 * t_w
+                    t_y_bar = t_y + 0.5 * t_h
 
-                if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (
-                        x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
-                    matchCarID = carID
+                    if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (
+                            x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
+                        matchCarID = carID
 
-            if matchCarID is None:
-                print('Creating new tracker ' + str(currentCarID))
+                if matchCarID is None:
+                    print('Creating new tracker ' + str(currentCarID))
 
-                tracker = dlib.correlation_tracker()
-                tracker.start_track(image, dlib.rectangle(x, y, x + w, y + h))
+                    tracker = dlib.correlation_tracker()
+                    tracker.start_track(image, dlib.rectangle(x, y, x + w, y + h))
 
-                carTracker[currentCarID] = tracker
-                carLocation1[currentCarID] = [x, y, w, h]
+                    carTracker[currentCarID] = tracker
+                    carLocation[currentCarID] = [x, y, w, h]
+                    speed[currentCarID] = None
 
-                currentCarID = currentCarID + 1
+                    currentCarID = currentCarID + 1
 
-        # cv2.line(resultImage,(0,480),(1280,480),(255,0,0),5)
+        # test traffic line classifier
+        showTrafficLines(lineTracker, image, resultImage)
 
+        # go through tracker, get predicted position from tracker and compare to previous position for estimated speed
         for carID in carTracker.keys():
             trackedPosition = carTracker[carID].get_position()
 
-            t_x = int(trackedPosition.left())
-            t_y = int(trackedPosition.top())
-            t_w = int(trackedPosition.width())
-            t_h = int(trackedPosition.height())
+            x2 = int(trackedPosition.left())
+            y2 = int(trackedPosition.top())
+            w2 = int(trackedPosition.width())
+            h2 = int(trackedPosition.height())
 
-            cv2.rectangle(resultImage, (t_x, t_y), (t_x + t_w, t_y + t_h), rectangleColor, 4)
+            cv2.rectangle(resultImage, (x2, y2), (x2+w2, y2+h2), rectangleColor, 4)
 
-            # speed estimation
-            carLocation2[carID] = [t_x, t_y, t_w, t_h]
+            [x1, y1, w1, h1] = carLocation[carID]
+            carLocation[carID] = [x2, y2, w2, h2]
 
-        cnts = getContourMap(image)
-        lineTracker = getTrafficLines(image)
+            if [x1, y1, w1, h1] != [x2, y2, w2, h2]:
+                # speed[carID] = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2])
+                speed[carID] = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2], lineTracker, image, cnts, resultImage)
 
-        # testing traffic line classifier
-        # testLines(lineTracker, image, resultImage)
+                if speed[carID]:
+                    cv2.putText(resultImage, str(int(speed[carID])) + " km/hr", (int(x1 + w1 / 2), int(y1 - 5)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
 
-        for i in carLocation1.keys():
-            if frameCounter % 1 == 0:
-                [x1, y1, w1, h1] = carLocation1[i]
-                [x2, y2, w2, h2] = carLocation2[i]
-
-                # print 'previous location: ' + str(carLocation1[i]) + ', current location: ' + str(carLocation2[i])
-                carLocation1[i] = [x2, y2, w2, h2]
-
-                # print 'new previous location: ' + str(carLocation1[i])
-                if [x1, y1, w1, h1] != [x2, y2, w2, h2]:
-                    # speed[i] = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2])
-                    speed[i] = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2], lineTracker, image, cnts, resultImage)
-
-                    # if y1 > 275 and y1 < 285:
-                    if speed[i]:
-                        cv2.putText(resultImage, str(int(speed[i])) + " km/hr", (int(x1 + w1 / 2), int(y1 - 5)),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
 
         cv2.imshow('result', resultImage)
 
@@ -246,6 +249,7 @@ def trackMultipleObjects():
         if cv2.waitKey(33) == 27:
             break
 
+        frameCounter = frameCounter + 1
 
     video.release()
     cv2.destroyAllWindows()
